ea45bbbd75ae1088fc556c549a1fb6c4441b2892
王昊 <yingxs888@126.com>
2018-07-10 12:37:07 +0800

    feat (zhi):初步完成简单的注册表单

---
+++ jQuery-ajax/zhi/from/main.js
@@ -0,0 +1,33 @@
+$(function(){
+
+	$('#search_button').button({
+		disabled : false,           //是否禁用
+		label : '搜索',               //按钮上的文字
+		icons :{
+			primary : 'ui-icon-search'                //按钮的前置图标
+			//secondary : 'ui-icon-triangle-1-s'      //按钮的后置图标
+		},
+		text:true,              //当设置为false时，不会显示文字但是必须要有一个指定的图标
+	});
+
+	$('#reg').dialog({
+		autoOpen:true,
+		modal:true,
+		resizable:false,
+		width:320,
+		height:340,
+		buttons : {
+			'提交':function(){
+
+			}
+		}
+	});
+
+	$('#reg').buttonset();
+	$('#date').datepicker();
+	$('#reg input[title]').tooltip();
+
+
+
+
+});
